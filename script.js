674f97711ed28289d405ce7d2c570d75a31292e2
Cagri <cagri-05-16@hotmail.com>
2026-05-02 11:45:57 +0300

    deneme

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const btn = document.getElementById('actionBtn');
+    
+    btn.addEventListener('click', () => {
+        alert('Tebrikler! JavaScript başarıyla çalışıyor.');
+    });
+});
